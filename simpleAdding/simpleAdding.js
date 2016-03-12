aef2545330125dc525ebb1668520216bbd888161
Hakan <hakan@kamcord.com>
2016-03-11 21:47:28 -0800

    simpleAdding question added

---
+++ simpleAdding/simpleAdding.js
@@ -0,0 +1,37 @@
+/* Given an input number a, write a function simpleAdding(a)
+that calculates the sum of all numbers 
+from 0 to a, a included. If a is undefined return 0, if a is
+not a number return 0. If a is a negative number return 0.If 
+a is not an integer, calculate the
+sum up to the nearest smaller integer. */
+
+/**
+Examples:
+
+var result = simpleAdding(3);
+console.log(result); // 6
+
+var result = simpleAdding();
+console.log(result); // 0
+
+var result = simpleAdding("number");
+console.log(result); // 0
+
+var result = simpleAdding([3,4,5]]);
+console.log(result); // 0
+
+var result = simpleAdding(5);
+console.log(result); // 15
+
+var result = simpleAdding(6.34);
+console.log(result); // 21
+
+var result = simpleAdding(-45);
+console.log(result); // 0
+
+**/
+
+function simpleAdding(a) {
+	//Code here
+
+}
