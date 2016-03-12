e3266e7c2129dd6f875559d390db566ae187ca3c
Hakan <hakan@kamcord.com>
2016-03-11 22:01:00 -0800

    Adding bonuses.

--- simpleAdding/simpleAdding.js
+++ simpleAdding/simpleAdding.js
@@ -1,15 +1,33 @@
 /* Given an input number a, write a function simpleAdding(a)
 that calculates the sum of all numbers 
-from 0 to a, a included. If a is undefined return 0, if a is
-not a number return 0. If a is a negative number return 0.If 
-a is not an integer, calculate the
-sum up to the nearest smaller integer. */
+from 0 to a, a included.
+ - If a is a negative number return 0.
+
+Bonus 1: If a is undefined return 0, if a is
+not a number return 0. Check out typeof() operator
+at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+
+Bonus 2: If a is not an integer, calculate the
+sum up to the nearest smaller integer. Check out 
+Math.floor() function at 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/floor.
+
+Alternatively, look into the remainder operator (%).
+e.g. console.log(7%3); // 1
+console.log(12%5) // 2
+console.log(1.3%1) // 0.3 */
 
 /**
 Examples:
 
 var result = simpleAdding(3);
 console.log(result); // 6
+
+var result = simpleAdding(5);
+console.log(result); // 15
+
+var result = simpleAdding(-45);
+console.log(result); // 0
 
 var result = simpleAdding();
 console.log(result); // 0
@@ -20,14 +38,8 @@ console.log(result); // 0
 var result = simpleAdding([3,4,5]]);
 console.log(result); // 0
 
-var result = simpleAdding(5);
-console.log(result); // 15
-
 var result = simpleAdding(6.34);
 console.log(result); // 21
-
-var result = simpleAdding(-45);
-console.log(result); // 0
 
 **/
 
