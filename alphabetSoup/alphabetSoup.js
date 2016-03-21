e6ab6f1932fddbf141345363ff6667a14895c51b
Hakan <hakan@kamcord.com>
2016-03-20 23:04:03 -0700

    new problem alphabetSoup

---
+++ alphabetSoup/alphabetSoup.js
@@ -0,0 +1,32 @@
+/* 
+Write a function alphabetSoup(str) that takes a string and returns a string with
+the letters in alphabetical order. Assume the input will not contain numbers or punctuation.
+Hint: Check out split(), sort() and join() methods at 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+
+Bonus:
+If there are spaces in the input, do not include them in the result.
+Hint: Check out trim() method at
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+*/
+
+/**
+Examples:
+
+var result = alphabetSoup("hello");
+console.log(result) // "ehllo"
+
+var result = alphabetSoup("");
+console.log(result) // ""
+
+var result = alphabetSoup("f");
+console.log(result) // "f"
+
+**/
+
+function alphabetSoup(str) {
+	//Your code here
+	
+}
