f67ce5f20e86b64c5fe9f7f9e8ffd28c622e2b2c
Hakan <hakan@kamcord.com>
2016-03-28 22:38:45 -0700

    adding dashInsert, swapCase, isPrime questions

---
+++ swapCase.js
@@ -0,0 +1,34 @@
+/*
+
+Given a string, swap the case of each character and retunr it.
+Assume the string has only letters.
+
+Hint 1: Check out indexOf() method at
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
+
+Hint 2: Check toLowerCase and toUpperCase methods at
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+Bonus: If the string has non-letter characters, leave them as they are.
+
+*/
+
+/**
+Examples:
+
+var result = swapCase("Hello World")
+console.log(result) // "hELLO wORLD"
+
+var result = swapCase("");
+console.log(result) // ""
+
+Bonus:
+var result = swapCase("Oh, Hello!!");
+console.log(result) // "oH, hELLO!!"
+
+**/
+
+function swapCase(str) {
+	// Your code
+}
