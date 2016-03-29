78fdcb99ff1ab2ec423fe8ad45a96629e7ab7886
Laura Hackney <laurahackney@Lauras-MacBook-Pro.local>
2016-03-28 19:18:37 -0700

    adding factorializeIt.js

---
+++ factorializeIt.js
@@ -0,0 +1,14 @@
+//Challenge time! 
+//Return the factorial of the provided integer.
+
+// If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n. Factorials are often represented with the shorthand notation n!
+
+// For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+// Check out Arithmetic Operators https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
+
+function factorializeIt(num) {
+  //your code here
+}
+
+factorializeIt(5);
