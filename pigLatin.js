2f19566763677e1672703a7fc4fdf16c1ec83b64
LauraH <laura@anniecannons.com>
2016-05-20 17:25:49 -0700

    adding pigLatin.js

---
+++ pigLatin.js
@@ -0,0 +1,21 @@
+//PIG LATIN!
+
+//Translate the provided string to pig latin.
+
+//Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
+
+//If a word begins with a vowel you just add "way" to the end.
+
+
+function translatePigLatin(str) {
+  //your code here
+}
+
+translatePigLatin("consonant");
+
+// TEST CASES
+// translatePigLatin("california") should return "aliforniacay".
+// translatePigLatin("paragraphs") should return "aragraphspay".
+// translatePigLatin("glove") should return "oveglay".
+// translatePigLatin("algorithm") should return "algorithmway".
+// translatePigLatin("eight") should return "eightway".
