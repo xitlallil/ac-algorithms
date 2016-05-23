e30ebc012927e06111fe79118310f005376e5750
LauraH <laura@anniecannons.com>
2016-05-22 20:18:41 -0700

    adding spinalCase.js

---
+++ spinalCase.js
@@ -0,0 +1,15 @@
+//Let's learn spinal case!!
+
+//Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+function spinalCase(str) {
+  // your code here
+}
+
+spinalCase('This Is Spinal Case');
+
+// spinalCase("This Is Spinal Case") should return "this-is-spinal-case".
+// spinalCase("thisIsSpinalCase") should return "this-is-spinal-case".
+// spinalCase("The_Andy_Griffith_Show") should return "the-andy-griffith-show".
+// spinalCase("Teletubbies say Eh-oh") should return "teletubbies-say-eh-oh".
+// spinalCase("AllThe-small Things") should return "all-the-small-things".
